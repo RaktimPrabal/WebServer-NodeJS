091ea27194d4b3189efe280bbe28e347e6b369aa
raktim prabal <raktimprabal@gmail.com>
2018-08-23 23:11:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express= require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+
+var app= express();
+
+hbs.registerPartials(__dirname+ '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+ '/public'));
+hbs.registerHelper('currentYear', ()=>{
+	return new Date().getFullYear();
+});
+
+app.use((req, res, next)=>{
+	var now = new Date().toString();
+	var log= `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + ' \n  ');
+next();
+});
+
+// app.use((req, res)=>{
+//  	res.render('maintenance.hbs');
+// });
+app.get('/', (req, res)=>{
+	res.render('home.hbs', {
+		pageTitle: 'WELCOME'
+	});
+});
+
+app.get('/about', (req, res)=>{
+	res.render('about.hbs', {
+		pageTitle: 'This is about' 
+	});
+});
+
+app.get('/bad', (req, res)=>{
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+app.listen(3000,()=>{
+	console.log("go to port 3000");
+});
